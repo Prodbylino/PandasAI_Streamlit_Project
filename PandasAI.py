6e731c29844728034a603e11b163eb55e4a605e7
Prodbylino <prodbylino@outlook.com>
2023-06-14 12:43:05 +1000

    fix bugs

--- PandasAI.py
+++ PandasAI.py
@@ -6,6 +6,8 @@ from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
 import matplotlib
 import matplotlib.pyplot as plt
+import chardet
+
 
 #hide style
 hide_style = """
@@ -32,16 +34,34 @@ if "uploaded_file" not in st.session_state:
     st.session_state["uploaded_file"] = None
 uploaded_file = st.file_uploader("File Type Restricted:.csv &.xlsx file only", type=['csv','xlsx'])
 
+
+
 # Check if a new file has been uploaded
 if uploaded_file is not None:
     st.session_state["uploaded_file"] = uploaded_file
 
 # If there is a file in the session state, display it
 if st.session_state["uploaded_file"] is not None:
-    df = pd.read_csv(st.session_state["uploaded_file"])
-    st.session_state["df"] = df 
-    st.write(df.head())
-    # Enter the prompt
+    # Detect file encoding
+    result = chardet.detect(st.session_state["uploaded_file"].read())
+    # Reset the file pointer
+    st.session_state["uploaded_file"].seek(0)
+    # Read file based on the file extension
+    try:
+        file_name = st.session_state["uploaded_file"].name
+        
+        # Check the file extension
+        if file_name.endswith('.csv'):
+            df = pd.read_csv(st.session_state["uploaded_file"], encoding=result['encoding'])
+        elif file_name.endswith('.xlsx'):
+            df = pd.read_excel(st.session_state["uploaded_file"], engine='openpyxl')
+
+        st.session_state["df"] = df
+        st.write("File loaded successfully.")
+        st.write(df.head())
+    except Exception as e:
+        st.write(f"Failed to load the file due to error: {e}")
+
     prompt = st.text_area("Enter your Prompt:")
 
     #Generate Answers 
